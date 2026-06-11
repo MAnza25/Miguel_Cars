e8f82c07726c9e6fa05caf503f5dbc12fdcea840
AndradeJ15 <andrade.jarol15@gmail.com>
2026-06-11 08:13:18 -0500

    Correcciones

--- backend-miguelcars/src/main/java/com/proyecto/Miguelcars/modelo/OrdenServicio.java
+++ backend-miguelcars/src/main/java/com/proyecto/Miguelcars/modelo/OrdenServicio.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Data
